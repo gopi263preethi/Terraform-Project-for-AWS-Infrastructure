6773c55397e6e199548a0b64283cdea1d15c521c
gopi263preethi <144430771+gopi263preethi@users.noreply.github.com>
2024-10-07 11:08:39 +0530

    Update compute.tf

--- Terraform_Porject/compute.tf
+++ Terraform_Porject/compute.tf
@@ -1,6 +1,12 @@
-variable "vpc_id" {}
-variable "subnet-id" {}
-variable "sg-id" {}
+variable "vpc_id" {
+    type = string
+}
+variable "subnet_id" { 
+     type = string 
+} 
+variable "sg_id" {
+type = string 
+}
   
 
 
@@ -10,8 +16,8 @@ variable "sg-id" {}
 resource "aws_instance" "sample_instance" {
     ami = "ami-022ce6f32988af5fa"
     instance_type = "t2.micro"
-    subnet_id = var.subnet-id
-    vpc_security_group_ids = var.sg-id
+    subnet_id = var.subnet_id
+    vpc_security_group_ids = [var.sg_id]
     key_name = "sample-mumbai-keypair"
 
     tags = {
